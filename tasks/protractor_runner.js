1e9f3f2e05a3bad79a7611935f751cdf3f323328
Lei Zhu <lzhu@walmart.com>
2015-02-02 14:57:34 -0800

    enable rerun and sauce

--- tasks/protractor_runner.js
+++ tasks/protractor_runner.js
@@ -13,11 +13,17 @@ var path = require('path');
 var fs = require('fs');
 var split = require('split');
 var through2 = require('through2');
+var q = require('q');
+var fs = require('fs');
+var _ = require('lodash');
 
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('protractor', 'A grunt task to run protractor.', function() {
-
+    var retryQueue = [];
+    var specs = [];
+    var retriedSpecs = {};
+    var counter = {};
     // '.../node_modules/protractor/lib/protractor.js'
     var protractorMainPath = require.resolve('protractor');
     // '.../node_modules/protractor/bin/protractor'
@@ -33,7 +39,9 @@ module.exports = function(grunt) {
       debug: false,
       nodeBin: 'node',
       args: {},
-      output: false
+      output: false,
+      retry: 0,
+      saucelab: false
     });
 
     // configFile is a special property which need not to be in options{} object.
@@ -50,27 +58,38 @@ module.exports = function(grunt) {
     var objectArgs = ["params", "capabilities", "cucumberOpts", "mochaOpts"];
 
     var args = process.execArgv.concat([protractorBinPath, opts.configFile]);
-    if (opts.noColor){
+    if (opts.noColor) {
       args.push('--no-jasmineNodeOpts.showColors');
     }
-    if (!grunt.util._.isUndefined(opts.debug) && opts.debug === true){
-      args.splice(1,0,'debug');
+    if (!grunt.util._.isUndefined(opts.debug) && opts.debug === true) {
+      args.splice(1, 0, 'debug');
     }
 
     // Iterate over all supported arguments.
     strArgs.forEach(function(a) {
       if (a in opts.args || grunt.option(a)) {
-        args.push('--'+a, grunt.option(a) || opts.args[a]);
+        args.push('--' + a, grunt.option(a) || opts.args[a]);
       }
     });
+
     listArgs.forEach(function(a) {
       if (a in opts.args || grunt.option(a)) {
-        args.push('--'+a,  grunt.option(a) || opts.args[a].join(","));
+        if (a === 'specs') {
+          var specFiles = fs.readdirSync(opts.args[a]);
+          specs = grunt.option(a) || _.map(specFiles, function(n) {
+            return opts.args[a] + '/' + n;
+          }).join(",");
+          // args.push('--' + a, grunt.option(a) || sf.join(","));
+        } else {
+          args.push('--' + a, grunt.option(a) || opts.args[a].join(","));
+        }
+
+
       }
     });
     boolArgs.forEach(function(a) {
       if (a in opts.args || grunt.option(a)) {
-        args.push('--'+a);
+        args.push('--' + a);
       }
     });
 
@@ -83,21 +102,21 @@ module.exports = function(grunt) {
           if (type === "object") {
             if (Array.isArray(val)) {
               // Add duplicates --[object].key val1 --[object].key val2 ...
-              for (var i=0;i<val.length;i++) {
-                args.push(prefix+"."+key, val[i]);
+              for (var i = 0; i < val.length; i++) {
+                args.push(prefix + "." + key, val[i]);
               }
             } else {
               // Dig deeper
-              convert(prefix+"."+key, val, args);
+              convert(prefix + "." + key, val, args);
             }
           } else if (type === "undefined" || type === "function") {
             // Skip these types
           } else if (type === "boolean") {
             // Add --[object].key
-            args.push(prefix+"."+key);
+            args.push(prefix + "." + key);
           } else {
             // Add --[object].key value
-            args.push(prefix+"."+key, val);
+            args.push(prefix + "." + key, val);
           }
         }
       })("--" + a, opts.args[a], args);
@@ -107,56 +126,126 @@ module.exports = function(grunt) {
 
     // Spawn protractor command
     var done = this.async();
-    var child = grunt.util.spawn({
-        cmd: opts.nodeBin,
-        args: args,
-        opts: {
-          stdio:'pipe'
-        }
-      },
-      function(error, result, code) {
-        if (error) {
-          grunt.log.error(String(result));
-          if(code === 1 && keepAlive) {
-            // Test fails but do not want to stop the grunt process.
-            grunt.log.oklns("Test failed but keep the grunt process alive.");
-            done();
-            done = null;
+
+
+    var promiseCb = function(specFile) {
+      return function() {
+        var vargs = _.clone(args);
+        vargs.push('--specs', specFile);
+
+        var cmd = {
+          cmd: opts.nodeBin,
+          args: vargs,
+          opts: {
+            stdio: 'pipe'
+          }
+        };
+
+        var def = q.defer();
+
+        var child = grunt.util.spawn(cmd, function(err, result, code) {
+
+          // console.log(err, code, retryQueue, retriedSpecs, opts.retry, specFile)
+          if (err) {
+            grunt.log.error(String(result));
+            if (code === 1 && keepAlive) {
+              // Test fails but do not want to stop the grunt process.
+              grunt.log.oklns("Test failed but keep the grunt process alive.");
+              if (opts.retry) {
+                if (!retriedSpecs[specFile]) {
+                  retriedSpecs[specFile] = 0;
+                }
+
+                if (retriedSpecs[specFile] < opts.retry) {
+                  console.log(retriedSpecs[specFile], opts.retry)
+                  // retryQueue.push(specFile);
+                  // retry is one and not meet the maximum attempt
+                  // increase retry attempt
+                  retriedSpecs[specFile] += 1;
+                  def.resolve(true, specFile);
+                } else {
+                  def.resolve(specFile);
+                }
+
+              } else {
+                def.resolve(specFile);
+              }
+
+            } else {
+              grunt.fail.fatal('protractor exited with code: ' + code, 3);
+              def.resolve(specFile);
+            }
           } else {
-            // Test fails and want to stop the grunt process,
-            // or protractor exited with other reason.
-            grunt.fail.fatal('protractor exited with code: '+code, 3);
+            def.resolve(specFile);
           }
-        } else {
-          done();
-          done = null;
+        });
+
+
+        process.stdin.pipe(child.stdin);
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
+        // Write the result in the output file
+        if (!grunt.util._.isUndefined(opts.output) && opts.output !== false) {
+
+          grunt.log.writeln("Output test result to: " + opts.output);
+
+          grunt.file.mkdir(path.dirname(opts.output));
+
+          child.stdout
+            .pipe(split())
+            .pipe(through2(function(chunk, encoding, callback) {
+              if ((/^Using the selenium server at/).test(chunk.toString())) {
+                // skip
+              } else {
+                this.push(chunk + '\n');
+              }
+              callback();
+            }))
+            .pipe(fs.createWriteStream(opts.output));
         }
+        return def.promise;
       }
-    );
-    process.stdin.pipe(child.stdin);
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-
-    // Write the result in the output file
-    if (!grunt.util._.isUndefined(opts.output) && opts.output !== false) {
-
-      grunt.log.writeln("Output test result to: " + opts.output);
-
-      grunt.file.mkdir(path.dirname(opts.output));
-
-      child.stdout
-        .pipe(split())
-        .pipe(through2(function (chunk, encoding, callback) {
-          if ((/^Using the selenium server at/).test(chunk.toString())) {
-            // skip
-          }
-          else {
-            this.push(chunk + '\n');
-          }  
-          callback();
-        }))
-        .pipe(fs.createWriteStream(opts.output));
     }
+
+    // build protractor cmd per spec
+    specs.split(',').forEach(function(spec) {
+      // init queue
+      retryQueue.push(spec);
+      counter[spec] = true;
+    });
+
+    if (opts.saucelab) {
+      // launch as saucelabs
+      // TODO: finish it
+    }
+
+    var chain = q();
+
+    var reChain = function(chain, spec) {
+      return chain
+        .then(promiseCb(spec))
+        .then(function(specFile) {
+          if (specFile === true) {
+            // retry the chain
+            console.log('-----------------------------> push', spec, 'back to queue');
+            return reChain(chain, spec);
+          } else {
+            // succeed or end test
+            delete counter[spec];
+            if (_.size(counter) === 0) {
+              // end up grunt process
+              done();
+            }
+          }
+        });
+    };
+
+    while (retryQueue.length > 0) {
+      var cmd = retryQueue.shift();
+      console.log('-----------------------------> push', cmd, 'to queue');
+      chain = reChain(chain, cmd);
+    }
+
   });
 
 };
